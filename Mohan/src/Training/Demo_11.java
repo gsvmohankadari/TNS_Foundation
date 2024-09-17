2dd890ba3a43a6d1a24e2f2c072a022776fb2c38
G.S.V.Mohan Kadari <G.S.V.Mohan Kadari@Mohan>
2024-09-17 22:35:34 +0530

    secound commit

---
+++ Mohan/src/Training/Demo_11.java
@@ -0,0 +1,10 @@
+package Training;
+
+public class Demo_11 {
+
+	public static void main(String[] args) {
+		System.out.println("mmoohhaann");
+
+	}
+
+}
---
+++ Mohan/src/Training/printing_numbers.java
@@ -0,0 +1,10 @@
+package Training;
+
+public class printing_numbers {
+
+	public static void main(String[] args) {
+		int number = 10;
+        System.out.println(number);
+	}
+
+}
